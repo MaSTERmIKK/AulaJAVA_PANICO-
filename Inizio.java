f28c63d4d2e263dfe4c1c68e27cd0d15c09dda5b
Mirko Campari <82829377+MaSTERmIKK@users.noreply.github.com>
2024-03-13 15:44:42 +0100

    primo commit

---
+++ Inizio.java
@@ -0,0 +1,13 @@
+public class Inizio {
+// il nome della classe è relativa  a dispositivo eletronico
+
+
+   String nameAPP ;  // serve e diversificare chi porta i valori
+
+
+   //risoluzione prima task
+   public String avviaApplicazione(String nameAP ){
+       System.out.println("L'applicazione si sta avviando e si chiama");
+       return nameAPP;
+   }   
+}
